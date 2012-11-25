a020d4bdec55ea4df693a7c9cf2a58d8ac821e2d
Celestial <comforter1989@yahoo.com.cn>
2012-11-25 21:09:19 +0800

    递归二分搜索

--- src/org/celestial/SectionFour_BinarySearch.java
+++ src/org/celestial/SectionFour_BinarySearch.java
@@ -20,9 +20,22 @@ public class SectionFour_BinarySearch {
 		return -1;
 	}
 	
+	public static int binarySearchRecursion(int[] arr, int num, int low, int high) {
+		if(arr.length == 0 || low > high)
+			return -1;
+		int mid = (low + high) / 2; 
+		if(num < arr[mid])
+			return binarySearchRecursion(arr, num, low, mid - 1);
+		else if(num > arr[mid])
+			return binarySearchRecursion(arr, num, mid + 1, high);
+		else
+			return mid;
+	}
+	
 	public static void main(String[] args) {
 		int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
 		System.out.println(binarySearch(arr, 4));
+		System.out.println(binarySearchRecursion(arr, 4, 0, arr.length - 1));
 	}
 
 }
